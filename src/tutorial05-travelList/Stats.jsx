bca5af5d816400ca9f95b60e58d30f97345e07c6
neoayus <ayus43@outlook.com>
2026-01-26 00:45:11 +0530

    implement stats calculation and conditional rendering

--- src/tutorial05-travelList/Stats.jsx
+++ src/tutorial05-travelList/Stats.jsx
@@ -1,15 +1,24 @@
 export default function Stats({ items }) {
+  // Conditional Rendering: 
+  if (!items.length){
+    return (
+      <p className="stats"> 
+        <em>Start adding things to your list, lessgooo </em>
+      </p>
+    )
+  }
   // derived state
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
 
   return (
+     
+
     <footer className="stats">
       <em>
-        {" "}
-        You have {numItems} items on your list, and you already packed{" "}
-        {numPacked} ({percentage}%)
+        {percentage === 100 ? ' You are all ready to go !!' : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+        
       </em>
     </footer>
   );
