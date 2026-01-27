108740bb6cfecf9bbaf37d506f39f4533f2a1ccf
neoayus <ayus43@outlook.com>
2026-01-27 18:40:50 +0530

    feat: add conditional rendering of 'add friend form'

--- src/tutorial06-eatNsplit/Split.jsx
+++ src/tutorial06-eatNsplit/Split.jsx
@@ -1,96 +1,118 @@
-import './Index.css' ; 
+import { useState } from "react";
+import "./Index.css";
 
-  // list of friends 
-  const initialFriends = [
-    {
-      id: 118836,
-      name: "Clark",
-      image: "https://i.pravatar.cc/48?u=118836",
-      balance: -7,
-    },
-    {
-      id: 933372,
-      name: "Sarah",
-      image: "https://i.pravatar.cc/48?u=933372",
-      balance: 20,
-    },
-    {
-      id: 499476,
-      name: "Anthony",
-      image: "https://i.pravatar.cc/48?u=499476",
-      balance: 0,
-    },
-  ];
-export default function Split(){
+// list of friends
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+export default function Split() {
+  const [showAddFriend, setShowFriend] = useState(false);
 
-  return(
+  // function addFriend(){ // action to be performed when ' Add Friend ' is clicked
+  //   console.log("add friend is clicked") ;
+  // }
+
+  return (
     <div className="app">
-      <div className='sidebar'>
+      <div className="sidebar">
         <FriendList />
-        <FormAddFriend />
-        <Button> Add Friend </Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button
+          onClick={() => {
+            setShowFriend((show) => !show);
+          }}
+        >
+          {showAddFriend ? "Close" : "Add Friend" }
+        </Button>
       </div>
-      <FormSplitBill /> 
-    </div> 
-  )
+      <FormSplitBill />
+    </div>
+  );
 }
 
-// SPLIT 
-function FriendList(){
-  let friends = initialFriends ;
+// SPLIT
+function FriendList() {
+  let friends = initialFriends;
 
-  return(
+  return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id}/>
+        <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
-  )
+  );
 }
 
-// SPLIT 
-export function Button({children}){
-  return(
-    <button className='button'>{children}</button>
-  )
+// SPLIT
+export function Button({ onClick, children }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
 }
 
-// SPLIT 
-function Friend({friend}){
-  return(
+// SPLIT
+function Friend({ friend }) {
+  return (
     <li>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
-      {friend.balance < 0 && <p className='red'> You Owe {friend.name} {friend.balance} $. </p> }
-      {friend.balance == 0 && <p> You and {friend.name} are even. </p> }
-      {friend.balance > 0 && <p className='green'> {friend.name} Owe You {friend.balance} $. </p> }
-      
-      <Button> Select </Button>      
+      {friend.balance < 0 && (
+        <p className="red">
+          {" "}
+          You Owe {friend.name} {friend.balance} $.{" "}
+        </p>
+      )}
+      {friend.balance == 0 && <p> You and {friend.name} are even. </p>}
+      {friend.balance > 0 && (
+        <p className="green">
+          {" "}
+          {friend.name} Owe You {friend.balance} $.{" "}
+        </p>
+      )}
+
+      <Button> Select </Button>
     </li>
-  )
+  );
 }
 
-
-// SPLIT 
-function FormAddFriend(){
-  return(
-    <form className='form-add-friend'>
-
+// SPLIT
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
       <label> Friend Name </label>
-      <input type="text"  />
+      <input type="text" />
 
       <label> Image URL </label>
-      <input type="text"  />
+      <input type="text" />
 
-      <Button> Add </Button>      
+      <Button> Add </Button>
     </form>
-  )
+  );
 }
 
-// SPLIT 
-function FormSplitBill({friend}){
-  return(
-    <form className='form-split-bill'>
+// SPLIT
+function FormSplitBill({ friend }) {
+  return (
+    <form className="form-split-bill">
       <h2>Split a Bill with X </h2>
 
       <label> Bill Value </label>
@@ -100,16 +122,15 @@ function FormSplitBill({friend}){
       <input type="number" />
 
       <label> X's Expanses </label>
-      <input type="number" disabled/>
+      <input type="number" disabled />
 
       <label>Who is paying the bill? </label>
       <select name="" id="">
         <option value="you"> You </option>
         <option value="friend"> X </option>
       </select>
-      
+
       <Button> Split Bill </Button>
-    
     </form>
-  )
+  );
 }
